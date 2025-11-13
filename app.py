3ec9efff7755fe3adf0ec36bb57c863fbd094540
= <=>
2025-11-13 12:58:27 +0530

    update

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__, template_folder='templates')
+
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+@app.route("/", methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        try:
+            s_len = float(request.form.get('s_len'))
+            s_width = float(request.form.get('s_width'))
+
+            p_len = float(request.form.get('p_len'))
+            p_width = float(request.form.get('p_width'))
+            
+            input_data = np.array([[s_len, s_width, p_len, p_width]])
+            X_scaled = scaler.transform(input_data)
+
+            pred = model.predict(X_scaled)
+            print(pred)
+            return render_template("index.html", result=f"Predicted Flower Species: {pred[0]}")
+        except Exception as e:
+            return render_template("index.html", result = f"Error: {e}")
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=False)
--- train.py
+++ train.py
@@ -1,6 +1,25 @@
-print("hello")
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import joblib
 
-# import pandas as pd
+df = pd.read_csv('Irisflower.csv')
 
-# df = pd.read_csv('Irisflower.csv')
-# print(df.head())
+df.dropna(inplace=True)
+
+X = df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = df['Species']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.25, random_state=42)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+joblib.dump(model, "model.pkl")
+joblib.dump(scaler, "scaler.pkl")
+
+print("Successful!")
